4467eeb8ede358b2732bde92dfa1a896395f9686
singhveer001 <veerbhansingh70@gmail.com>
2024-04-09 15:02:31 +0530

    storybook in react

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { RevenueCard } from './components/RevenueCard'
+import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
+    <>
+    <Navbar/>
     <div className='grid grid-cols-4 m-5 gap-7' >
         <RevenueCard  title={"Amount Pending"} amount={"2,312.23"} orderCount={23} isFirstCard={true} />
         <RevenueCard  title={"Amount Pending"} amount={"92,312.20"} orderCount={13} />
         <RevenueCard   title={"Amount Pending"} amount={"92,312.20"}  />
     </div>
+    </>
   )
 }
 
---
+++ src/stories/components/RevenueCard.stories.jsx
@@ -0,0 +1,23 @@
+import { RevenueCard } from "../../components/RevenueCard";
+
+const meta = {
+    component:RevenueCard,
+}
+
+export default meta;
+
+export const BigAmount = {
+  args: {
+    orderCount: "132",
+    title:"asd",
+    amount :"1000000000"
+  },
+};
+
+export const SmallAmount = {
+    args: {
+      orderCount: "132",
+      title:"asd",
+      amount :"10000"
+    },
+  };
